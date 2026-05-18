b4c1ff2d463e2b190680a2061532ed415ab3c6d4
Yz-kaguya <251205110@ccmailg.meijo-u.ac.jp>
2026-05-18 14:17:34 +0900

    色を変更

--- js/tetris.js
+++ js/tetris.js
@@ -8,21 +8,15 @@ var currentX, currentY; // position of current shape
 var freezed; // is current shape settled on the board?
 var shapes = [
     [ 1, 1, 1, 1 ],
-    [ 1, 1, 1, 0,
-      1 ],
-    [ 1, 1, 1, 0,
-      0, 0, 1 ],
-    [ 1, 1, 0, 0,
-      1, 1 ],
-    [ 1, 1, 0, 0,
-      0, 1, 1 ],
-    [ 0, 1, 1, 0,
-      1, 1 ],
-    [ 0, 1, 0, 0,
-      1, 1, 1 ]
+    [ 1, 1, 1, 0,1 ],
+    [ 1, 1, 1, 0,0, 0, 1 ],
+    [ 1, 1, 0, 0,1, 1 ],
+    [ 1, 1, 0, 0,0, 1, 1 ],
+    [ 0, 1, 1, 0,1, 1 ],
+    [ 0, 1, 0, 0,1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'orange', 'blue', 'yellow', 'red', 'lime', 'purple'
+    'cyan', 'blue', 'aqua', 'skyblue', 'fuchsia', 'plum', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
